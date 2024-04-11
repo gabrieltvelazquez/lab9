783631bb2e22ea51eb7c906cd987d8a8498ae470
Burge <burgerito78@gmail.com>
2024-04-11 10:46:18 -0400

    Added decoder

--- encoder.py
+++ encoder.py
@@ -3,3 +3,9 @@ def encoder(password):
     for i in password:
         encoded += encoded + str(int(i)+3)
     return encoded
+
+def decoder(password):
+    decoded = ""
+    for i in password:
+        decoded += decoded + str(int(i) - 3)
+    return decoded
